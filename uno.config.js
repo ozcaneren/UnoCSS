0a6e24e6ed450b10f2d865daa0dbb3cd319dac73
eren ozcan <erenozcandev@gmail.com>
2023-05-03 16:57:13 +0300

    first

--- uno.config.js
+++ uno.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
+import presetUno from '@unocss/preset-uno'
+import presetAttributify from '@unocss/preset-attributify'
+import transformerAttributifyJsx from '@unocss/transformer-attributify-jsx'
 
 export default defineConfig({
-  // ...UnoCSS options
+  presets: [
+    presetUno(),
+    presetAttributify({ /* preset options */ }),
+    // ...
+  ],
+  transformers: [
+    transformerAttributifyJsx(), // <--
+  ],
 })
